2d14d28c8f58a647ea130a7ec5a50458fad12dee
Marilyne Naili <marilyne.lydia@gmail.com>
2023-03-01 16:54:12 +0100

    marilyne

--- algo3.php
+++ algo3.php
@@ -2,7 +2,40 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
 function getCost($volume)
 {
+    $prix = 0;
 
+    if ($volume <=100) {
+        $prix = $volume*1;
+        return $prix;
+    }
+
+    if ($volume <=150) {
+        $prix = (1*100) + (0.98*($volume-100));
+        return $prix;
+    }
+    if ($volume <=175) {
+        $prix = (1*100) + (0.98*50) + (0.95*($volume-150));
+        return $prix;
+    }
+    if ($volume <=200) {
+        $prix = (1*100) + (0.98*50) + (0.95*25) + (0.90*($volume-175));
+        return $prix;
+    }
+    if ($volume >=500) {
+
+    }
 }
+
+var_dump(getCost(125));
--- algo4.php
+++ algo4.php
@@ -7,8 +7,6 @@ CN3: sumIntegers(-1) => "false"
 CN4: sumIntegers(3) => "6"
 CN5: sumIntegers(5) => "15"
 CN6: sumIntegers(9) => "45"
-
-
 */
 
 function sumIntegers($nombre)
@@ -32,7 +30,7 @@ function sumIntegers($nombre)
 
 function testSumIntegers1()
 {
-    
+
     if (!sumIntegers(0)) {
         echo "Le test est réussie";
     } else {
