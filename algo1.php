63ed9287ce23a971cddc2c8f145c487258c938f2
PODER Elias <elias.poderr@gmail.com>
2023-03-01 12:45:50 +0100

    test 4

--- algo1.php
+++ algo1.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-CN1: mean(4, 1) => "2.8"
+CN1: mean(4, 1) => "2.5"
 CN2: mean(8, 5) => "6.5"
-CN3: mean(5, 2) => "4.3"
-CN4: mean(2, 19) => "10.05"
+CN3: mean(5, 2) => "3.5"
+CN4: mean(3, 19) => "11"
 */
 
 function mean($nbr1, $nbr2)
@@ -12,9 +12,11 @@ function mean($nbr1, $nbr2)
     return ($nbr1 + $nbr2) / 2;
 }
 
-echo "CN1: mean(4, 1) => '2.8'".PHP_EOL;
+echo "CN1: mean(4, 1) => '2.5'".PHP_EOL;
 var_dump(mean(4, 1));
 echo "CN2: mean(8, 5) => '6.5'".PHP_EOL;
 var_dump(mean(8, 5));
-echo "CN3: mean(5, 2) => '4.3'".PHP_EOL;
+echo "CN3: mean(5, 2) => '3.5'".PHP_EOL;
 var_dump(mean(5, 2));
+echo "CN4: mean(3, 19) => '11'".PHP_EOL;
+var_dump(mean(3, 19));
