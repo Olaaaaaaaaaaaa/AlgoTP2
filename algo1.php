50c8e134562dfeace39092295fc301c3a3a38a4c
Marilyne Naili <marilyne.lydia@gmail.com>
2023-03-01 12:39:26 +0100

    test 3

--- algo1.php
+++ algo1.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
-CN1: mean(4, 1.6) => "2.8"
+CN1: mean(4, 1) => "2.8"
 CN2: mean(8, 5) => "6.5"
-CN3: mean(5.9, 2.7) => "4.3"
-CN4: mean(1.1, 19) => "10.05"
+CN3: mean(5, 2) => "4.3"
+CN4: mean(2, 19) => "10.05"
 */
 
 function mean($nbr1, $nbr2)
@@ -12,7 +12,9 @@ function mean($nbr1, $nbr2)
     return ($nbr1 + $nbr2) / 2;
 }
 
-echo "CN1: mean(4, 1.6) => '2.8'".PHP_EOL;
-var_dump(mean(4, 1.6));
-echo "CN1: mean(8, 5) => '6.5'".PHP_EOL;
+echo "CN1: mean(4, 1) => '2.8'".PHP_EOL;
+var_dump(mean(4, 1));
+echo "CN2: mean(8, 5) => '6.5'".PHP_EOL;
 var_dump(mean(8, 5));
+echo "CN3: mean(5, 2) => '4.3'".PHP_EOL;
+var_dump(mean(5, 2));
