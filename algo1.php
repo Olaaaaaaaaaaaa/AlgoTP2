3a3a22435415245e8233e4a501a5dae6dfbb5dd9
Marilyne Naili <marilyne.lydia@gmail.com>
2023-03-01 12:23:03 +0100

    TDD

--- algo1.php
+++ algo1.php
@@ -1,18 +1,8 @@
 <?php
 
-
-function mean ($nbr1, $nbr2)
-{
-    $moyenne = ($nbr1 + $nbr2) / 2;
-
-    return $moyenne;
-}
-
-echo "NBR1= 2 | NBR2=2 | RESULTAT= 2".PHP_EOL;
-var_dump(mean(2,2));
-echo "NBR1= 5 | NBR2=7 | RESULTAT= 6".PHP_EOL;
-var_dump(mean(5,7));
-echo "NBR1= 9 | NBR2=8 | RESULTAT= 8.5".PHP_EOL;
-var_dump(mean(9,8));
-echo "NBR1= 2 | NBR2=23 | RESULTAT= 12.5".PHP_EOL;
-var_dump(mean(2,23));
+/*
+CN1: mean(4, 1.6) => "2.8"
+CN2: mean(8, 5) => "6.5"
+CN3: mean(5.9, 2.7) => "4.3"
+CN4: mean(1.1, 19) => "10.05"
+*/
