a8228f2b6f51e55a99faa0c625689c1090da58e6
PODER Elias <elias.poderr@gmail.com>
2023-03-01 15:01:15 +0100

    Squelette

--- algo4.php
+++ algo4.php
@@ -1,2 +1,26 @@
 <?php
 
+/*
+CN1: sumIntegers(0) => "false"
+CN2: sumIntegers(2.2) => "false"
+CN3: sumIntegers(-1) => "false"
+CN4: sumIntegers(3) => "6"
+CN5: sumIntegers(5) => "15"
+CN6: sumIntegers(9) => "45"
+
+
+*/
+
+function sumIntegers($nombre)
+{
+    
+}
+
+function testSumIntegers1()
+{
+    if (!sumIntegers(0)) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
