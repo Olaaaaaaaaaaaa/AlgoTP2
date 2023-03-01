fcfff941dd23f12eaed657a69f1badc3041a2a5e
PODER Elias <elias.poderr@gmail.com>
2023-03-01 14:16:58 +0100

    algo2

--- algo2.php
+++ algo2.php
@@ -1,37 +1,28 @@
 <?php
 
+/*
+CN1: mention(21) => "false"
+CN2: mention(-1) => "false"
+CN3: mention(0) => ""
+CN4: mention(8) => ""
+CN4: mention(12) => "AB"
+CN4: mention(15) => "Bien"
+CN4: mention(16) => "TB"
+CN4: mention(19) => "Excellent"
+*/
+
 function mention($note)
 {
-    if ($note < 0 || $note > 20){
-        return false;
-    }
+    
+}
 
-    if ($note >= 18){
-        return "Excellent";
-    }elseif ($note >= 16){
-        return "TB";
-    }elseif ($note >= 14){
-        return "Bien";
-    }elseif ($note >= 12){
-        return "AB";
-    }else{
-        return "";
+function testMention21()
+{
+    if (!mention(21)) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
     }
 }
 
-echo "note=21  | RESULTAT= false".PHP_EOL;
-var_dump(mention(21));
-echo "note= -1 | RESULTAT= false".PHP_EOL;
-var_dump(mention(-1));
-echo "note= 0 | RESULTAT= ".PHP_EOL;
-var_dump(mention(0));
-echo "note= 8 | RESULTAT= ".PHP_EOL;
-var_dump(mention(8));
-echo "note= 12 | RESULTAT= AB".PHP_EOL;
-var_dump(mention(12));
-echo "note= 15 | RESULTAT= Bien".PHP_EOL;
-var_dump(mention(15));
-echo "note= 16 | RESULTAT= TB".PHP_EOL;
-var_dump(mention(16));
-echo "note= 19 | RESULTAT= Excellent".PHP_EOL;
-var_dump(mention(19));
+testMention21();
