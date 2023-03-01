1e1ab4edd653938f5566fd650fe492c9a29f58a9
PODER Elias <elias.poderr@gmail.com>
2023-03-01 12:28:02 +0100

    squelette fonction

--- algo1.php
+++ algo1.php
@@ -6,3 +6,11 @@ CN2: mean(8, 5) => "6.5"
 CN3: mean(5.9, 2.7) => "4.3"
 CN4: mean(1.1, 19) => "10.05"
 */
+
+function mean($nbr1, $nbr2)
+{
+    return false;
+}
+
+echo "CN1: mean(4, 1.6) => '2.8'".PHP_EOL;
+var_dump(mean(4, 1.6));
