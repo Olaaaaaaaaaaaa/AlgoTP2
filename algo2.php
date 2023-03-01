519de5b0a63a95cdb98121330089ebbed0626039
PODER Elias <elias.poderr@gmail.com>
2023-03-01 14:32:50 +0100

    Pour Marilyne le reuf qui est encore un peu malade

--- algo2.php
+++ algo2.php
@@ -3,26 +3,97 @@
 /*
 CN1: mention(21) => "false"
 CN2: mention(-1) => "false"
-CN3: mention(0) => ""
-CN4: mention(8) => ""
-CN4: mention(12) => "AB"
-CN4: mention(15) => "Bien"
-CN4: mention(16) => "TB"
-CN4: mention(19) => "Excellent"
+CN3: mention(0) => "Pas de mention"
+CN4: mention(8) => "Pas de mention"
+CN5: mention(12) => "AB"
+CN6: mention(15) => "Bien"
+CN7: mention(16) => "TB"
+CN8: mention(19) => "Excellent"
 */
 
 function mention($note)
 {
-    
+
 }
 
 function testMention21()
 {
-    if (!mention(21)) {
+    if (mention(21) == false) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention_1()
+{
+    if (mention(-1) == false) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention0()
+{
+    if (mention(0) == "Pas de mention") {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention8()
+{
+    if (mention(8) == "Pas de mention") {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention12()
+{
+    if (mention(12) == "AB") {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention15()
+{
+    if (mention(15) == "Bien") {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention16()
+{
+    if (mention(16) == "TB") {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function testMention19()
+{
+    if (mention(19) == "Excellent") {
         echo "Le test est réussie";
     } else {
     echo  "Le test n'est pas réussie";
     }
 }
 
+echo PHP_EOL."Test 1: ".PHP_EOL;
 testMention21();
+echo PHP_EOL."Test 2: ".PHP_EOL;
+testMention_1();
+echo PHP_EOL."Test 3: ".PHP_EOL;
+testMention0();
+echo PHP_EOL."Test 4: ".PHP_EOL;
+testMention8();
+echo PHP_EOL."Test 5: ".PHP_EOL;
+testMention12();
+echo PHP_EOL."Test 6: ".PHP_EOL;
+testMention15();
+echo PHP_EOL."Test 7: ".PHP_EOL;
+testMention16();
+echo PHP_EOL."Test 8: ".PHP_EOL;
+testMention19();
