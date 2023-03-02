f5be8ec3c165aa781151e2f709451822b57da2c3
PODER Elias <elias.poderr@gmail.com>
2023-03-02 09:52:18 +0100

    Squelette et Test algo5

---
+++ algo5.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+CN1: fibonacci1(-1) => "false"
+CN2: fibonacci2(2.2) => "false"
+CN3: fibonacci3(0) => "0"
+CN4: fibonacci4(1) => "1"
+CN5: fibonacci5(2) => "1"
+CN6: fibonacci6(5) => "5"
+CN7: fibonacci7(9) => "34"
+CN8: fibonacci8(15) => "610"
+*/
+
+
+function fibonacci($n)
+{
+
+}
+
+function fibonacci1()
+{
+    if (!getCost(-1)) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci2()
+{
+    if (!getCost(2.2)) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci3()
+{
+    if (getCost(0) == 0) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci4()
+{
+    if (getCost(1) == 1) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci5()
+{
+    if (getCost(2) == 1) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci6()
+{
+    if (getCost(5) == 5) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci7()
+{
+    if (getCost(9) == 34) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
+function fibonacci8()
+{
+    if (getCost(15) == 610) {
+        echo "Le test est réussie";
+    } else {
+    echo  "Le test n'est pas réussie";
+    }
+}
